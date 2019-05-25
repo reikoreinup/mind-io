bb667ba9cbb05c8a297a7bed6d6ecb7bf260735b
Reiko-Rainer <rreinu@ttu.ee>
2019-05-26 00:41:49 +0300

    Initial commit

---
+++ public/script.js
@@ -0,0 +1,210 @@
+$(document).ready(function () {
+
+    var socket = io.connect();
+    var strings = [
+        " got ahead of themselves.",
+        " is too eager for this game.",
+        " was too fast for the rest.",
+        " misclicked, probably...",
+        " is trying to sabotage you!",
+        " doesn't have a strong grasp on numbers yet.",
+        ", hold your horses!"];
+    var username;
+    var currentRoom;
+    var round = 1;
+    var lives = 4;
+
+    $("#userForm").submit(function (e) {
+        e.preventDefault();
+        username = $("#username").val();
+        socket.emit("new user", username);
+    });
+
+    $("#joinGameForm").submit(function (e) {
+        e.preventDefault();
+        socket.emit("join room", $("#roomID").val());
+    });
+
+    $("#startGameButton").click(function (e) {
+        e.preventDefault();
+        round = 1;
+        lives = 4;
+        socket.emit("round started", {round: round, lives: lives, room: currentRoom})
+    });
+
+    $("#leaveRoomButton").click(function (e) {
+        e.preventDefault();
+        socket.emit("leave room", currentRoom);
+        $("#chooseActionArea").show();
+        $("#waitingArea").hide();
+    });
+
+    $("#backToLoginButton").click(function (e) {
+        e.preventDefault();
+        socket.emit("remove user", username);
+        $("#username").val("");
+        $("#chooseActionArea").hide();
+        $("#loginArea").show();
+    });
+
+    $(".yourNumbers").on("click", ".number", function(e){
+        let clickedNumber = $(e.target).text();
+        socket.emit("clicked number", {room: currentRoom, number: clickedNumber})
+    });
+
+    $("#restartRoundButton").click(function (e) {
+        e.preventDefault();
+        socket.emit("hide overlay", currentRoom);
+        socket.emit("round started", {round: round, lives: lives, room: currentRoom})
+
+    });
+
+    $("#returnToLobbyButton").click(function (e) {
+        e.preventDefault();
+        socket.emit("hide overlay", currentRoom);
+        socket.emit("clear game data", currentRoom);
+        socket.emit("join room", currentRoom);
+
+    });
+
+    $("#nextRoundButton").click(function (e) {
+        e.preventDefault();
+        socket.emit("hide overlay", currentRoom);
+        if (round % 2 === 0) {
+            lives++;
+        }
+        round++;
+        socket.emit("round started", {round: round, lives: lives, room: currentRoom})
+    });
+
+
+
+    socket.on("new user", data => {
+        if (data) {
+            $("#loginArea").hide();
+            $("#chooseActionArea").show();
+        } else {
+            alert("This name is taken! Choose another.")
+        }
+    });
+
+    socket.on("join room", function (data) {
+        let room = data.room;
+        let successful = data.roomAvailable;
+
+
+        if (successful) {
+            currentRoom = room;
+            $("#chooseActionArea").hide();
+            $("#waitingArea").show();
+            $("#roomName").html("Room name: " + currentRoom);
+        } else {
+            alert("Can't join this room. Game in progress")
+        }
+    });
+
+    socket.on("user joined", function (usernames) {
+        let content = "";
+        $("#gameArea").hide();
+        $("#waitingArea").show();
+        for (let i = 0; i < usernames.length; i++) {
+            content += "<li class='list-group-item'>" + usernames[i] + "</li>"
+        }
+        $("#users").html(content);
+    });
+
+
+    socket.on("round started", function (data) {
+        let myNumbers = data.numbers;
+        lives = data.lives;
+        round = data.round;
+        let players = data.players;
+
+
+        let myNumbersContent = "";
+        let othersNamesContent = "";
+        let othersCardsContent = "";
+
+        $("#waitingArea").hide();
+        $("#gameArea").show();
+        $("#currentCard").html("--").css("background-color", "white");
+        for (let i = 0; i < myNumbers.length; i++) {
+            myNumbersContent += "<span class='number p-1 m-1'>" + myNumbers[i] + "</span>";
+        }
+
+        for (let i = 0; i < players.length; i++) {
+            if (players[i] !== username) {
+                let cardEmoji = "🎴";
+                othersNamesContent += "<th>" + players[i] + "</th>";
+                othersCardsContent += "<td>" + cardEmoji.repeat(round)+ "</td>";
+            }
+        }
+
+        $("#roundInfo").html(`Lives: ${lives}\nRound: ${round}`);
+        $(".yourNumbers").html(myNumbersContent);
+        $("#amountOfCards").html(othersCardsContent);
+        $("#userNames").html(othersNamesContent);
+
+    });
+
+    socket.on("clicked number", function (data) {
+        let number = data.number;
+        let username = data.username;
+        let correct = data.correct;
+        let $currentCard = $("#currentCard");
+
+        $("#lastCard").html(username + ":");
+        $currentCard.html(number);
+        removeCardIconByName(username);
+        if (correct) {
+            console.log(username + ": " + number + " -> correct number")
+        } else {
+            console.log(username + ": " + number + " -> wrong number");
+            $currentCard.css("background-color", "red" );
+        }
+
+    });
+
+
+    socket.on("remove from hand", function (number) {
+        $(".yourNumbers").children("span").each(function () {
+            if (number === $(this).text()) {
+                $(this).remove()
+            }
+        });
+    });
+
+    socket.on("retry", function (data) {
+        let username = data.username;
+        lives = data.lives;
+        $("#overlay").show();
+        $("#text").html(username + strings[Math.floor(Math.random()*strings.length)]);
+    });
+
+    socket.on("hide overlay", function () {
+        $("#overlay").hide();
+        $("#endOverlay").hide();
+        $("#nextRoundOverlay").hide();
+    });
+
+    socket.on("game over", function (username) {
+        $("#endOverlay").show();
+        $("#textFinal").html(username + strings[Math.floor(Math.random()*strings.length)] +
+        `\nYou reached round ${round}.`);
+
+    });
+
+    socket.on("next round", function () {
+        $("#nextRoundOverlay").show();
+        if (round % 2 === 0) {
+            $("#textNextRound").html("You passed this round! Have an extra life for the effort.");
+        } else {
+            $("#textNextRound").html("You passed this round!");
+        }
+    });
+
+    function removeCardIconByName(username) {
+
+    }
+
+});
---
+++ server.js
@@ -0,0 +1,175 @@
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io").listen(server);
+
+
+app.use(express.static(__dirname + '/node_modules'));
+app.use(express.static(__dirname + '/public'));
+
+users = [];
+connections = [];
+badRooms = [];
+gameData = {};
+
+const port = 6969;
+server.listen(port);
+console.log("Server is running on port " + port);
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "public/index.html")
+});
+
+io.sockets.on('connection', function (socket) {
+    connections.push(socket);
+    console.log("connected: %s sockets connected", connections.length);
+
+    socket.on('disconnect', function (data) {
+
+        users.splice(users.indexOf(socket.username), 1);
+
+        connections.splice(connections.indexOf(socket), 1);
+        console.log("disconnected: %s sockets connected", connections.length);
+    });
+
+    socket.on('new user', function (data) {
+        if (!users.includes(data)) {
+            socket.username = data;
+            users.push(socket.username);
+            socket.emit("new user", true)
+        } else {
+            socket.emit("new user", false)
+        }
+
+    });
+
+    socket.on('join room', function (room) {
+        if (!Object.keys(gameData).includes(room)) {
+            socket.join(room);
+            let usernamesInRoom = getUsersInRoom(room);
+            socket.emit('join room', {room: room, roomAvailable: true});
+            io.to(room).emit('user joined', usernamesInRoom);
+        } else {
+            socket.emit('join room', {room: room, roomAvailable: false});
+        }
+    });
+
+    socket.on('game started', function (currentRoom) {
+
+        io.to(currentRoom).emit('game started');
+    });
+
+    socket.on('round started', function (data) {
+        let round = data.round;
+        let room = data.room;
+        let lives = data.lives;
+
+
+        let clients = io.sockets.adapter.rooms[room].sockets;
+        let numClients = (typeof clients !== 'undefined') ? Object.keys(clients).length : 0;
+
+        let numbers = dealNumbers(round, numClients);
+        gameData[room] = {currentNumbers: flatten(numbers), lives: lives};
+        console.log(gameData);
+        console.log(numbers);
+        console.log("round: " + round + "\nplayers: " + numClients);
+        let i = 0;
+
+        for (let clientId in clients) {
+            let clientSocket = io.sockets.connected[clientId];
+            clientSocket.emit('round started', {numbers: numbers[i], lives: lives, round: round, players: getUsersInRoom(room)});
+            i++;
+        }
+
+    });
+
+    socket.on('clicked number', function (data) {
+        let room = data.room;
+        let number = data.number;
+        let username = socket.username;
+
+        if (isSmallest(number, room)) {
+            io.to(room).emit('clicked number', {number: number, username: username, correct: true});
+            gameData[room]["currentNumbers"].splice(gameData[room]["currentNumbers"].indexOf(parseInt(number)), 1);
+            if (gameData[room]["currentNumbers"].length === 0) {
+                io.to(room).emit('next round');
+            }
+        } else {
+            io.to(room).emit('clicked number', {number: number, username: username, correct: false});
+            gameData[room].lives--;
+            console.log('remaining lives: ' + gameData[room].lives);
+            if (gameData[room].lives === 0) {
+                io.to(room).emit('game over', username);
+            } else {
+                io.to(room).emit('retry', {username: username, lives: gameData[room].lives})
+            }
+        }
+        socket.emit('remove from hand', number);
+
+    });
+
+
+    socket.on('hide overlay', function (room) {
+        io.to(room).emit('hide overlay');
+    });
+
+    socket.on('clear game data', function (room) {
+        delete gameData[room];
+    });
+
+    socket.on('remove user', function (username) {
+        users.splice(users.indexOf(username), 1);
+    });
+
+    socket.on('leave room', function (room) {
+        socket.leave(room);
+        io.to(room).emit('user joined', getUsersInRoom(room));
+    })
+
+
+});
+
+function isSmallest(number, room) {
+    let numbers = gameData[room]["currentNumbers"];
+    let minNumber = Math.min(...numbers);
+    console.log("Clicked: " + number + "\nMinNumber: " + minNumber);
+    return minNumber === parseInt(number);
+
+}
+
+function dealNumbers(round, amountOfPlayers) {
+    let allNumbers = [];
+    let result = [];
+    for (let i = 0; i < round * amountOfPlayers; i++) {
+        let num = Math.floor(Math.random() * 100) + 1;
+        while (allNumbers.includes(num)) {
+            num = Math.floor(Math.random() * 100) + 1;
+        }
+        allNumbers.push(num);
+    }
+    for (let i = 0; i < allNumbers.length; i += round) {
+        result.push(allNumbers.slice(i, i + round))
+    }
+
+    return result;
+
+
+}
+
+function getUsersInRoom(room) {
+    try {
+        let clients = io.sockets.adapter.rooms[room].sockets;
+        let usernamesInRoom = [];
+        for (let clientID in clients) {
+            usernamesInRoom.push(io.sockets.connected[clientID].username);
+        }
+        return usernamesInRoom;
+    } catch (TypeError) {
+        return [];
+    }
+}
+
+function flatten(arr) {
+    return [].concat(...arr)
+}
+
